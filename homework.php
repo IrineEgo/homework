a23f3fcbbb69cc7a6f47121896a10f75a13d01f8
Irine Egorenkova <17315833+IrineEgo@users.noreply.github.com>
2018-10-12 09:15:17 +0300

    Update homework.php

--- homework.php
+++ homework.php
@@ -1,32 +1,31 @@
+<?php
+    $x = rand(0,100);
+    $var1 = 1;
+    $var2 = 1;
+	
+    while ($var1 < $x) {
+        $var3 = $var1;
+        $var1 = $var1 + $var2;
+        $var2 = $var3;
+    }
+?>
 <html lang="ru">
   <head>
     <meta charset="utf-8">
     <title>Дополнительное задание</title>
   </head>
   <body>
-  <h1>Дополнительное задание</h1>
-  <?php
-      $x = rand(0,100);
-      $var1 = 1;
-      $var2 = 1;
-          echo "Число: ".$x;
-	      echo "<br>";
+    <h1>Дополнительное задание</h1>
+    <?php
+        echo 'Число: '.$x;
+	echo "<br>";
 
-       while ($var1 <= $x) {
-           if ($var1 == $x) {
-               echo "задуманное число входит в числовой ряд";
-               break;
-           } 
-	       else {
-               $var3 = $var1;
-               $var1 = $var1 + $var2;
-               $var2 = $var3;
-           }
-           if ($var1 > $x) {
-               echo "задуманное число НЕ входит в числовой ряд";
-               break;
-           }
-       }
-   ?>
+        if ($var1 > $x) {
+            echo 'задуманное число НЕ входит в числовой ряд';
+        }    
+	    else if (var1 == $x) {
+	        echo 'задуманное число входит в числовой ряд';
+            }		   
+    ?>
   </body>
 </html>
